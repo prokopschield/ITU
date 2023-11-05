5084e01889bd996373a3ad0cacc9fc097f9bbf11
Tomas-Parizek <parizektom@email.cz>
2023-11-05 19:20:53 +0100

    xxx

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import Overview from "./Ucastnici/overview.svelte";
 	import MoveButton from "./MoveButton.svelte";
 	import Participant from "./Vedouci/ParticipantOverview.svelte";
+	import EditAction from "./Vedouci/EditAction.svelte"
 	const pkg = {
 		destination: Overview,
 		text:'Log Účastník'
@@ -12,6 +13,7 @@
 	<h1>IS CAMP</h1>
 	<MoveButton {...pkg}/>
 	<MoveButton destination = {Participant} text = {"Log Vedoucí"} />
+	<MoveButton destination = {EditAction} text = {"Editace události"} />
 </main>
 
 <style>
--- src/Vedouci/EditAction.svelte
+++ src/Vedouci/EditAction.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
     let buttons = ["Akce", "Účastníci", "Vedoucí"]
-    let participants: {name: String; points: Number; comment: String;}[] = [];
+    let participants: {name: String; points: Number; comment: String;}[] = [{name: "a", points: 5, comment: "ahoj"}, {name: "a", points: 5, comment: "ahoj"}];
 </script>
+
 <main id="main">
-    <header id="outer-header">
-        {#each buttons as button (button)}
-            <button>{button}</button>
-        {/each}
+    <header id="header">
+        <div>
+            {#each buttons as button (button)}
+                <button>{button}</button>
+            {/each}
+        </div>
         <img alt="UserIcon" id="user">
     </header>
     <div id="upper-div">
         <table id="settings">
             <tr>
-                <td>Jméno:<input></td>
-                <td>Datum/Čas:<input></td>
+                <td><input placeholder="Jméno"></td>
+                <td><input placeholder="Datum / Čas"></td>
                 <td>
                     <table>
                         <tr>
@@ -30,12 +33,11 @@
                         </tr>
                     </table>
                 </td>
-                <td>Maximum bodů:<input></td>
+                <td><input placeholder="Maximum bodů"></td>
             </tr>
             <tr>
                 <td colspan="3">
-                    Popis:
-                    <input>
+                    <input placeholder="Popis">
                 </td>
                 <td>
                     <input type="submit">
@@ -46,15 +48,15 @@
     <div id="lower-div">
         <table id="list">
             <thead>
-                <th>Jméno</th>
-                <th>Body</th>
-                <th>Komentář</th>
+                <th class="column0">Jméno</th>
+                <th class="column1">Body</th>
+                <th class="column2">Komentář</th>
             </thead>
             {#each participants as {name, points, comment}}
                 <tr>
-                    <td>{name}</td>
-                    <td>{points}</td>
-                    <td>{comment}</td>
+                    <td class="column0">{name}</td>
+                    <td class="column1">{points}</td>
+                    <td class="column2">{comment}</td>
                 </tr>
             {/each}
         </table>
@@ -62,60 +64,90 @@
 </main>
 
 <style>
-    #outer-header
+    #main
     {
-        height: 30px;
-        border: 1px solid black;
         position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
+    #main #header
+    {
+        position: absolute;
         top: 0;
         left: 0;
         right: 0;
     }
 
-    #main
+    #main #header div
     {
-        position: fixed;
-        bottom: 0;
-        top: 30;
-        left: 0;
-        right: 0;
-        border: 1px solid black;
+        position: absolute;
+        left: 5px;
+    }
+
+    #main #header img
+    {
+        position: absolute;
+        right: 5px;
     }
 
     #upper-div
     {
-        position: fixed;
+        position: absolute;
         left: 0;
         right: 0;
-        top: 30px;
-        height: 150px;
+        top: 40px;
         border: 1px solid black;
+        bottom: calc(100% - 200px);
+    }
+
+    #upper-div #settings
+    {
+        position: absolute;
+        width: 100%;
+    }
+
+    #upper-div #settings input:not([type])
+    {
+        width: calc(100% - 28px);
+        height: 100%;
+        font-size: 25px;
+    }
+
+    #upper-div #settings input[type='submit']
+    {
+        width: calc(100% - 20px);
+        height: 100%;
+        font-size: 25px;
     }
 
     #lower-div
     {
-        position: fixed;
+        position: absolute;
         left: 0;
         right: 0;
         bottom: 0;
-        top: 180px;
+        top: 200px;
         border: 1px solid black;
     }
 
-    #settings, #list
+    #lower-div #list
     {
         width: 100%;
-        height: 100%;
     }
-
-    #list thead
+    
+    .column0
     {
-        height: 30px;
+        width: 10%;
     }
-
-    input:not([type]), input[type='submit']
+    .column1
     {
-        width: 100%;
-        height: 30px;
+        width: 10%;
+    }
+    .column2
+    {
+        width: 80%;
     }
 </style>
