bd8d1ddf678a3f7523559375e5e724c65833e5ea
Prokop <prokop.schield@protonmail.com>
2023-11-05 11:39:19 +0100

    feat(src/lib/backend.ts): add auth-related backend handles

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,3 +1,60 @@
 import { createClient } from "@prokopschield/simple-socket-client";
 
 export const [client, socket] = createClient("wss://camp.fitvut.cz");
+
+export const backend = new Proxy(client, {
+	get(target, key) {
+		const real = target[String(key)];
+
+		return async (...args: any[]) => {
+			const { success, error, ...rest } = await real(...args);
+
+			if (error) {
+				throw error;
+			}
+
+			return rest;
+		};
+	},
+});
+
+export const login: (
+	username: string,
+	secret: string
+) => Promise<{
+	token: string;
+	username: string;
+	displayname: string;
+	legal_name: string;
+	legal_guardian: string;
+	legal_guardian_contact: string;
+	email: string;
+}> = backend.login;
+
+export const session: (
+	username: string,
+	token: string
+) => Promise<{
+	token: string;
+	username: string;
+	displayname: string;
+	legal_name: string;
+	legal_guardian: string;
+	legal_guardian_contact: string;
+	email: string;
+}> = backend.login;
+
+export const change_my_info: (info: {
+	displayname?: string;
+	legal_name?: string;
+	legal_guardian?: string;
+	legal_guardian_contact?: string;
+	email?: string;
+	redirect?: string;
+}) => Promise<{
+	username: string;
+	displayname: string;
+	legal_name: string;
+	legal_guardian: string;
+	legal_guardian_contact: string;
+}> = backend.change_my_info;
