237ca409cf8b75f9dd17d60c3c6d3049be605d4b
Tomas-Parizek <parizektom@email.cz>
2023-11-05 18:12:56 +0100

    xxx

--- src/Vedouci/ParticipantOverview.svelte
+++ src/Vedouci/ParticipantOverview.svelte
@@ -1,109 +1,147 @@
 <script lang="ts">
     let buttons = ["Akce", "Účastníci", "Vedoucí"]
-    let tableItems: { name: string; team: string; points: number; }[] = [];
+    let tableItems: { name: string; team: string; points: number; }[] = [{name:"a", team:"b", points:5}, {name:"a", team:"b", points:5}];
 </script>
 
-<main>
-    <header id="outer-header">
-        {#each buttons as button (button)}
-            <button>{button}</button>
-        {/each}
+<main id="main">
+    <header id="header">
+        <div>
+            {#each buttons as button (button)}
+                <button>{button}</button>
+            {/each}
+        </div>
         <img alt="UserIcon" id="user">
     </header>
-    <main id="outer-main">
-        <button>
-            Rozdělení bodů
-        </button>
-        <main id="inner-main">
-            <header id="inner-header">
-                <span id="column0">
-                    <span id="column0">Jméno</span>
-                    <span><img alt="sorting option"></span>
-                </span>
-                <span id="column1">
-                    <span id="column1">Tým</span>
-                    <span><img alt="sorting option"></span>
-                </span>
-                <span id="column2">
-                    <span>Body</span>
-                    <span><img alt="sorting option"></span>
-                </span>
-                <span id="column3">
-                    <img alt="sorting option">
-                </span>
-            </header>
-            <main>
-                {#each tableItems as {name, team, points}}
-                    <div>
-                        <span>{name}</span>
-                        <span>{team}</span>
-                        <span>{points}</span>
-                        <span>
-                            <img alt="chat">
-                            <img alt="upravit">
-                            <img alt="vymazat">
-                        </span>
-                    </div>
-                {/each}
-            </main>
-        </main>
+
+    <main id="inner-main">
+        <button>Rozdělení bodů</button>
+        <header id="inner-header">
+            <span id="column0">
+                <span>Jméno</span>
+                <span><img alt="sorting option"></span>
+            </span>
+            <span id="column1">
+                <span>Tým</span>
+                <span><img alt="sorting option"></span>
+            </span>
+            <span id="column2">
+                <span>Body</span>
+                <span><img alt="sorting option"></span>
+            </span>
+            <span id="column3">
+                <img alt="sorting option">
+            </span>
+        </header>
+        <table id="table">
+            {#each tableItems as {name, team, points}}
+                <tr class="item">
+                    <td class="column0">{name}</td>
+                    <td class="column1">{team}</td>
+                    <td class="column2">{points}</td>
+                    <td class="column3">
+                        <img alt="chat">
+                        <img alt="upravit">
+                        <img alt="vymazat">
+                    </td>
+                </tr>
+            {/each}
+        </table>
     </main>
 </main>
-
 <style>
-    #outer-header
+    #main
     {
-        height: 30px;
-        border: 1px solid black;
         position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
+    #main #header
+    {
+        position: absolute;
         top: 0;
         left: 0;
         right: 0;
     }
 
-    #outer-main
+    #main #header div
     {
-        position: fixed;
-        bottom: 0;
-        top: 30;
-        left: 0;
-        right: 0;
-        border: 1px solid black;
+        position: absolute;
+        left: 5px;
+    }
+
+    #main #header img
+    {
+        position: absolute;
+        right: 5px;
     }
 
     #inner-main
     {
-        position: fixed;
-        bottom: 30px;
-        top: 80px;
-        left: 20px;
-        right: 20px;
-        border: 1px solid black;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 40px;
+        bottom: 0;
+    }
+    #inner-main button
+    {
+        position: absolute;
+        top: 5px;
+        left: 5px;
     }
 
     #inner-header
     {
         position: relative;
-        top: 0;
+        top: 45px;
         left: 0;
         right: 0;
         height: 30px;
-        border: 1px solid black;
+    }
+    #table
+    {
+        position: relative;
+        top: 65px;
+        width: 100%;
+    }
+    .item
+    {
+        position: relative;
+    }
+    #column0
+    {
+        position: absolute;
+        left: 0;
+        right: 50%;
+    }
+    #column1
+    {
+        position: absolute;
+        left: 50%;
+        right: calc(50% - (50% / 3));
+    }
+    #column2
+    {
+        position: absolute;
+        left: calc(50% + (50% / 3));
+        right: calc(50% - (50% / 3) * 2);
+    }
+    #column3
+    {
+        position: absolute;
+        left: calc(50% + (50% / 3) * 2);
+        right: 0;
     }
 
-    #column0
+    .column0
     {
         width: 50%;
     }
-    
-    #column1, #column2, #column3
+    .column1, .column2, .column3
     {
         width: calc(50% / 3);
-    }
-
-    #user
-    {
-        position: absolute;
-        right: 5px;
     }
 </style>
