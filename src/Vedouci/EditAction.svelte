278835b6af6c7d2092d0a516aea02c63d5a3d361
Tomas-Parizek <parizektom@email.cz>
2023-11-05 16:34:22 +0100

    Něco z vedoucích

---
+++ src/Vedouci/EditAction.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+    let buttons = ["Akce", "Účastníci", "Vedoucí"]
+    let participants: {name: String; points: Number; comment: String;}[] = [];
+</script>
+<main id="main">
+    <header id="outer-header">
+        {#each buttons as button (button)}
+            <button>{button}</button>
+        {/each}
+        <img alt="UserIcon" id="user">
+    </header>
+    <div id="upper-div">
+        <table id="settings">
+            <tr>
+                <td>Jméno:<input></td>
+                <td>Datum/Čas:<input></td>
+                <td>
+                    <table>
+                        <tr>
+                            <td>Týmová</td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>Opakující se</td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                        <tr>
+                            <td>Volitelná</td>
+                            <td><input type="checkbox"></td>
+                        </tr>
+                    </table>
+                </td>
+                <td>Maximum bodů:<input></td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    Popis:
+                    <input>
+                </td>
+                <td>
+                    <input type="submit">
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div id="lower-div">
+        <table id="list">
+            <thead>
+                <th>Jméno</th>
+                <th>Body</th>
+                <th>Komentář</th>
+            </thead>
+            {#each participants as {name, points, comment}}
+                <tr>
+                    <td>{name}</td>
+                    <td>{points}</td>
+                    <td>{comment}</td>
+                </tr>
+            {/each}
+        </table>
+    </div>
+</main>
+
+<style>
+    #outer-header
+    {
+        height: 30px;
+        border: 1px solid black;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    #main
+    {
+        position: fixed;
+        bottom: 0;
+        top: 30;
+        left: 0;
+        right: 0;
+        border: 1px solid black;
+    }
+
+    #upper-div
+    {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 30px;
+        height: 150px;
+        border: 1px solid black;
+    }
+
+    #lower-div
+    {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: 180px;
+        border: 1px solid black;
+    }
+
+    #settings, #list
+    {
+        width: 100%;
+        height: 100%;
+    }
+
+    #list thead
+    {
+        height: 30px;
+    }
+
+    input:not([type]), input[type='submit']
+    {
+        width: 100%;
+        height: 30px;
+    }
+</style>
---
+++ src/Vedouci/ParticipantOverview.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+    let buttons = ["Akce", "Účastníci", "Vedoucí"]
+    let tableItems: { name: string; team: string; points: number; }[] = [];
+</script>
+
+<main>
+    <header id="outer-header">
+        {#each buttons as button (button)}
+            <button>{button}</button>
+        {/each}
+        <img alt="UserIcon" id="user">
+    </header>
+    <main id="outer-main">
+        <button>
+            Rozdělení bodů
+        </button>
+        <main id="inner-main">
+            <header id="inner-header">
+                <span id="column0">
+                    <span id="column0">Jméno</span>
+                    <span><img alt="sorting option"></span>
+                </span>
+                <span id="column1">
+                    <span id="column1">Tým</span>
+                    <span><img alt="sorting option"></span>
+                </span>
+                <span id="column2">
+                    <span>Body</span>
+                    <span><img alt="sorting option"></span>
+                </span>
+                <span id="column3">
+                    <img alt="sorting option">
+                </span>
+            </header>
+            <main>
+                {#each tableItems as {name, team, points}}
+                    <div>
+                        <span>{name}</span>
+                        <span>{team}</span>
+                        <span>{points}</span>
+                        <span>
+                            <img alt="chat">
+                            <img alt="upravit">
+                            <img alt="vymazat">
+                        </span>
+                    </div>
+                {/each}
+            </main>
+        </main>
+    </main>
+</main>
+
+<style>
+    #outer-header
+    {
+        height: 30px;
+        border: 1px solid black;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    #outer-main
+    {
+        position: fixed;
+        bottom: 0;
+        top: 30;
+        left: 0;
+        right: 0;
+        border: 1px solid black;
+    }
+
+    #inner-main
+    {
+        position: fixed;
+        bottom: 30px;
+        top: 80px;
+        left: 20px;
+        right: 20px;
+        border: 1px solid black;
+    }
+
+    #inner-header
+    {
+        position: relative;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 30px;
+        border: 1px solid black;
+    }
+
+    #column0
+    {
+        width: 50%;
+    }
+    
+    #column1, #column2, #column3
+    {
+        width: calc(50% / 3);
+    }
+
+    #user
+    {
+        position: absolute;
+        right: 5px;
+    }
+</style>
