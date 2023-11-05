39fd5793826c9aa0a31a47ed3244a6c53776abf9
Prokop <prokop.schield@protonmail.com>
2023-11-05 20:26:55 +0100

    feat(src/lib/state.ts): implement persistant authentication

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -2,6 +2,26 @@ import { writable } from "svelte/store";
 
 export const username = writable("");
 export const displayname = writable("");
-export const token = writable("");
+export const token = writable(localStorage.getItem("token") || "");
 
 export const page = writable("");
+
+let page_value = "";
+let redirect_to = "";
+
+page.subscribe((value) => (page_value = value));
+
+token.subscribe((value) => {
+	if (value) {
+		if (page_value === "Auth") {
+			page.set(redirect_to);
+		}
+	} else {
+		if (page_value !== "Auth") {
+			redirect_to = page_value;
+			page.set("Auth");
+		}
+	}
+
+	localStorage.setItem("token", value);
+});
