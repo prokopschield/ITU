36b50e9013c8517dabd32055c4eea9c25b812f2d
Prokop <prokop.schield@protonmail.com>
2023-12-14 14:13:02 +0100

    chore(src/lib/state.ts): add state() export

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -6,6 +6,7 @@ import { session } from "./backend.js";
 export const username = store<string>("username");
 export const displayname = store<string>("displayname");
 export const token = store<string>("token");
+export const state = (key: string) => store<number|string>(key);
 
 export const user = store<{
 	id: number | string;
