2b217f2c36323c821697246ae20c94f2d4acd991
Tomas-Parizek <parizektom@email.cz>
2023-12-16 10:46:25 +0100

    Řazení v jedné tabulce

--- src/pages/Vedouci/OverviewTable.svelte
+++ src/pages/Vedouci/OverviewTable.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
     let tableItems: { name: string; points: number }[] = [
 		{ name: "a", points: 5 },
         { name: "a", points: 5 },
@@ -31,15 +33,42 @@
         { name: "a", points: 5 },
         { name: "a", points: 5 },
 	];
+
+    let sortColumn: String | undefined = undefined;
+    let sortDirection = true; // ascending
+
+    function sortBy(column: String) {
+        if (sortColumn === column) { // Otočení směru řazení
+            sortDirection = !sortDirection;
+        } else {
+            sortColumn = column;
+            sortDirection = true;
+        }
+
+        tableItems = tableItems.sort((a, b) => {
+            const aValue = column === "name" ? a.name : a.points;
+            const bValue = column === "name" ? b.name : b.points;
+
+            if (sortDirection === true) {
+                return aValue > bValue ? 1 : -1;
+            } else {
+                return aValue < bValue ? 1 : -1;
+            }
+        });
+    }
+
+    onMount(() => {
+        sortBy("name");
+    })
 </script>
 
 <table id="table">
     <thead>
-        <th class="column0">
+        <th class="column0" on:click={() => sortBy("name")}>
             <span>Jméno</span>
             <span><img alt="sorting option" /></span>
         </th>
-        <th class="column2">
+        <th class="column2" on:click={() => sortBy("points")}>
             <span>Body</span>
             <span><img alt="sorting option" /></span>
         </th>
